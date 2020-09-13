4996e4b3d81465f11e96fc6c94b8b529c837616d
王仁兴 <allenwang@kugou.net>
2020-09-13 23:19:05 +0800

    init.

---
+++ src/1.getting_started/2.1.hello_triangle/hello_triangle.cpp
@@ -0,0 +1,227 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void onRender();
+bool doInitBeforeGLRenderLoop();
+void doReleaseAfterRenderLoop();
+
+GLfloat verticies[] = {
+    0.5f,  0.0f, 0.0f,
+    0.0f,  0.5,  0.0f,
+    0.0f, -0.5f, 0.0f,
+    -0.5f, 0.0f, 0.0f,
+    -0.5f,-0.5f, 0.0f
+};
+GLuint indices[] = {
+    1, 2, 3,
+    2, 3, 4
+};
+GLuint shaderProgram;
+
+const GLchar *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\n";
+const GLchar *fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n"
+;
+
+/**
+ * 一般当你打算绘制多个物体时，你首先要生成/配置所有的VAO（和必须的VBO及属性指针)，然后储存它们供后面使用。
+ * 当我们打算绘制物体的时候就拿出相应的VAO，绑定它，绘制完物体后，再解绑VAO。
+ * */
+GLuint vbo, ebo, vao;
+
+bool doInitBeforeGLRenderLoop() {
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if( !success ) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        return false;
+    }
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if( !success ) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FIALED\n" << infoLog << std::endl;
+        return false;
+    }
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if( !success ) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::LINK_PROGRAM_FAILED\n" << infoLog << std::endl;
+        return false;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    // 顶点数组对象(Vertex Array Object, VAO)
+    glGenVertexArrays(1, &vao);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    // 绑定vao，任何随后的顶点属性调用都会储存在这个VAO中
+    glBindVertexArray(vao);
+
+    // 顶点缓冲对象，GPU内存(通常被称为显存)中储存大量顶点
+    glGenBuffers(1, &vbo);
+    // 使用的任何（在GL_ARRAY_BUFFER目标上的）缓冲调用都会用来配置当前绑定的缓冲(VBO)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // 复制顶点数组到缓冲中供OpenGL使用，即复制到上述绑定的vbo
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    // 复制我们的索引数组到一个索引缓冲中，供OpenGL使用
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    // 通过glVertexAttribPointer设置的顶点属性配置，进行的顶点缓冲对象与顶点属性链接
+    // vao中attribute_pointer_0指向当前绑定的vbo
+    // vao中element_buffer_object指向当前绑定的ebo
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    // 要想使用VAO，要做的只是使用glBindVertexArray绑定VAO。从绑定之后起，应该绑定和配置对应的VBO和属性指针，之后解绑VAO供之后使用
+    // 解绑vao
+    glBindVertexArray(0);
+    // 小心把下面的解绑定放在vao解绑之前，导致属性设置为空
+    // 解绑vbo
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // 解绑ebo
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    return true;
+}
+
+void onRender() {
+    glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // 2. 当我们渲染一个物体时要使用着色器程序
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    // 3. 绘制物体
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0); // 没必要每次都解绑，可注释掉
+}
+
+void doReleaseAfterRenderLoop() {
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteProgram(shaderProgram);
+}
+
+
+int main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // do something before render loop
+    // do something before render loop
+    if( !doInitBeforeGLRenderLoop() ) {
+        std::cout << "Failed to call doInitBeforeGLRenderLoop" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        onRender();
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    doReleaseAfterRenderLoop();
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
